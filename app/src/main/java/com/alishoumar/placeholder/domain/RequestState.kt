29c1d355524c155192d75a5c84ac965ef521fce2
shoumarali <alialakbarshoumar610@gmail.com>
2024-06-14 07:30:37 +0300

    Initial

---
+++ app/src/main/java/com/alishoumar/placeholder/domain/RequestState.kt
@@ -0,0 +1,11 @@
+package com.alishoumar.placeholder.domain
+
+
+sealed class RequestState<out T> {
+
+    object Idle: RequestState<Nothing>()
+    object Loading: RequestState<Nothing>()
+    data class Success<T>(val data:T): RequestState<T>()
+    data class Error(val exception: Throwable): RequestState<Nothing>()
+
+}
---
+++ app/src/main/java/com/alishoumar/placeholder/presentation/components/NavGraph.kt
@@ -0,0 +1,9 @@
+package com.alishoumar.placeholder.presentation.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SetUpNavGraph() {
+
+
+}
